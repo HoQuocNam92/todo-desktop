1efe578a1ac3165cfb51f6a6c278df188e8b5956
hoquocnam92 <hoquocnam92@gmail.com>
2025-07-26 21:13:23 +0700

    upgrade

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
+const { autoUpdater } = require('electron-updater');
 const path = require("path");
-
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
@@ -18,7 +18,10 @@ function createWindow() {
         win.webContents.send("request-notification-permission");
     });
 }
-
+app.on('ready', () => {
+    createWindow();
+    autoUpdater.checkForUpdatesAndNotify(); // Tự kiểm tra khi mở app
+});
 app.whenReady().then(createWindow);
 
 ipcMain.handle("get-app-version", () => {
